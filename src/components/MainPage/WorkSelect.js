dce7fd0bda49c0fb3cbdf221452491be1e3b4eca
IpshironDelta <ysueda0716@gmail.com>
2024-01-11 17:01:49 +0900

    一般公開用に修正

--- src/components/MainPage/WorkSelect.js
+++ src/components/MainPage/WorkSelect.js
@@ -18,34 +18,14 @@ export default function BasicSelect(props) {
           value        = {props.value}
           label        = {props.label}
           onChange     = {props.onChange}>
-            <MenuItem value="検査・梱包作業（単独）">検査・梱包作業（単独）</MenuItem>
-            <MenuItem value="検査・梱包作業（複数人）">検査・梱包作業（複数人）</MenuItem>
-            <MenuItem value="検査・ヘッダー付き">検査・ヘッダー付き</MenuItem>
-            <MenuItem value="検査・圧着">検査・圧着</MenuItem>
-            <MenuItem value="検査・化粧箱">検査・化粧箱</MenuItem>
-            <MenuItem value="検査・ダンボール">検査・ダンボール</MenuItem>
-            <MenuItem value="検査・ラベル">検査・ラベル</MenuItem>
-            <MenuItem value="検査・作業・その他">検査・作業・その他</MenuItem>
-            <MenuItem value="検査・評価・ダンボール組立">検査・評価・ダンボール組立</MenuItem>
-            <MenuItem value="検査・評価・タンク梱包">検査・評価・タンク梱包</MenuItem>
-            <MenuItem value="物流・棚入れ・在庫補充リスト作成">物流・棚入れ・在庫補充リスト作成</MenuItem>
-            <MenuItem value="物流・棚入れ・在庫補充（整頓）">物流・棚入れ・在庫補充（整頓）</MenuItem>
-            <MenuItem value="物流・棚入れ・製品入庫（高層）">物流・棚入れ・製品入庫（高層）</MenuItem>
-            <MenuItem value="物流・棚入れ・製品入庫（パレット）">物流・棚入れ・製品入庫（パレット）</MenuItem>
-            <MenuItem value="物流・作業・その他">物流・作業・その他</MenuItem>
-            <MenuItem value="物流・出荷・全般作業">物流・出荷・全般作業</MenuItem>
-            <MenuItem value="物流・出荷・単品">物流・出荷・単品</MenuItem>
-            <MenuItem value="物流・出荷・同梱">物流・出荷・同梱</MenuItem>
-            <MenuItem value="物流・出荷・業者 株式会社HAMASHO">物流・出荷・業者 株式会社HAMASHO</MenuItem>
-            <MenuItem value="物流・出荷・業者 BOGMOTORCYCLE">物流・出荷・業者 BOGMOTORCYCLE</MenuItem>
-            <MenuItem value="物流・出荷・業者 株式会社リバークレイン">物流・出荷・業者 株式会社リバークレイン</MenuItem>
-            <MenuItem value="物流・出荷・業者 その他">物流・出荷・業者 その他</MenuItem>
-            <MenuItem value="物流・伝票仕分け">物流・伝票仕分け</MenuItem>
-            <MenuItem value="物流・評価・ハンドパレット">物流・評価・ハンドパレット</MenuItem>
-            <MenuItem value="物流・評価・ピッキング">物流・評価・ピッキング</MenuItem>
-            <MenuItem value="事務所・作業・その他">事務所・作業・その他</MenuItem>
-            <MenuItem value="事務所・経理・その他">事務所・経理・その他</MenuItem>
-            <MenuItem value="主任・作業・その他">主任・作業・その他</MenuItem>
+            <MenuItem value="入庫作業">入庫作業</MenuItem>
+            <MenuItem value="受け入れ作業">受け入れ作業</MenuItem>
+            <MenuItem value="棚卸作業">棚卸作業</MenuItem>
+            <MenuItem value="廃棄作業">廃棄作業</MenuItem>
+            <MenuItem value="出荷作業">出荷作業</MenuItem>
+            <MenuItem value="事務作業">事務作業</MenuItem>
+            <MenuItem value="経理作業">経理作業</MenuItem>
+            <MenuItem value="検品作業">検品作業</MenuItem>
         </Select>
         <FormHelperText>入力必須</FormHelperText>
       </FormControl>
